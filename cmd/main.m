c874b1825b84dfba40b71ec0164d55a4a2716608
jinghewang <jinghewang@163.com>
2016-11-14 09:23:15 +0800

    cmd init commit

---
+++ cmd/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cmd
+//
+//  Created by hbd on 16/11/14.
+//  Copyright © 2016年 hbd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
